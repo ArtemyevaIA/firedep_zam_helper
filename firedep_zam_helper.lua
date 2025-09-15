2071789f45dc9aa21199d32f20e18a8ea669bc34
ArtemyevaIA <nickart.artemyev@gmail.com>
2025-09-15 17:56:19 +0700

    Ver.15.09.A2

--- firedep_zam_helper.lua
+++ firedep_zam_helper.lua
@@ -72,20 +72,20 @@ function main()
         end
     end
     
-    -- local spisok_org=io.open(download,"r")
-    -- if spisok_org~=nil then
-    --     io.close(spisok_org)
-    -- else 
-    --     sampAddChatMessage('Список сотрудников не найден. Сейчас подгрузим.', -255)
-    --     downloadUrlToFile(url, download)
-    --     wait(3000)
-    --     sampShowDialog(0, "{FFA500}Список сотрудников ПД", "{78dbe2}Список сотруднков не был найден в папке с Вашей игрой. Я скачал его автоматически.\nПерезайдите в игру, для применения изменений.", "Перезайти", "", DIALOG_STYLE_MSGBOX)
-    --     while sampIsDialogActive(0) do wait(100) end
-    --     local result, button, _, input = sampHasDialogRespond(0)
-    --     if button == 1 then
-    --         sampProcessChatInput('/q', -1)
-    --     end
-    -- end
+    local spisok_org=io.open(download,"r")
+    if spisok_org~=nil then
+        io.close(spisok_org)
+    else 
+        sampAddChatMessage('Список сотрудников не найден. Сейчас подгрузим.', -255)
+        downloadUrlToFile(url, download)
+        wait(3000)
+        sampShowDialog(0, "{FFA500}Список сотрудников ПД", "{78dbe2}Список сотруднков не был найден в папке с Вашей игрой. Я скачал его автоматически.\nПерезайдите в игру, для применения изменений.", "Перезайти", "", DIALOG_STYLE_MSGBOX)
+        while sampIsDialogActive(0) do wait(100) end
+        local result, button, _, input = sampHasDialogRespond(0)
+        if button == 1 then
+            sampProcessChatInput('/q', -1)
+        end
+    end
 
     if autoupdate_loaded and enable_autoupdate and Update then
         pcall(Update.check, Update.json_url, Update.prefix, Update.url)
