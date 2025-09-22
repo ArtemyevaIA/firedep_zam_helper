0a5ea12474b7ea28d6da56d12b5ee77ec0179860
ArtemyevaIA <nickart.artemyev@gmail.com>
2025-09-22 09:44:40 +0700

    Ver.22.09.A2

--- firedep_zam_helper.lua
+++ firedep_zam_helper.lua
@@ -1,5 +1,5 @@
 script_name("firedep_zam_helper")
-script_version("Ver.22.09.A1")
+script_version("Ver.22.09.A2")
 
 local download = getGameDirectory()..'\\moonloader\\config\\firedep_zam_helper.lua.ini'
 local url = 'https://github.com/ArtemyevaIA/firedep_zam_helper/raw/refs/heads/main/firedep_zam_helper.lua.ini'
